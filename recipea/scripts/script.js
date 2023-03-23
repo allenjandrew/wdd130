f2a1cca20309ab8ce1aac1bd0bded32047cc2115
allenjandrew <allenjandrew@gmail.com>
2023-03-22 23:20:37 -0600

    lots of progress!

--- recipea/scripts/script.js
+++ recipea/scripts/script.js
@@ -7,8 +7,6 @@ function setResults() {
   reviResults.innerHTML = `<div>Result1</div><div>Result2</div><div>Result3</div>`;
 }
 
-window.addEventListener("load", loadRecipesJSON());
-
 // document.getElementById("infl").addEventListener("change", function () {
 //   var file_reader = new FileReader();
 //   file_reader.onload = function () {
@@ -17,28 +15,62 @@ window.addEventListener("load", loadRecipesJSON());
 //   file_reader.readAsText(this.files[0]);
 // });
 
-function loadRecipesJSON() {
+function usingJSON(link) {
   const xhttp = new XMLHttpRequest();
   xhttp.onload = function () {
-    myObj = JSON.parse(this.responseText);
+    var myObj = JSON.parse(this.responseText);
+    // This is where I can use the JSON object.
+
     console.log(myObj);
     fillFaveWidget(myObj);
+
+    thumbnails = document.getElementsByClassName("recipe-thumbnail");
+    for (item of thumbnails) {
+      item.onclick = function () {
+        modal.style.display = "block";
+        let recipe = myObj.recipes[this.dataset.recipeid];
+        modalDiv.innerHTML = JSON.stringify(recipe);
+      };
+    }
   };
-  xhttp.open(
-    "GET",
-    "https://allenjandrew.github.io/wdd130/recipea/recipes/recipes.json"
-  );
+  xhttp.open("GET", link);
   xhttp.send();
 }
 
 function fillFaveWidget(json) {
   var htmlString = "";
-  for (item of json.recipes) {
-    if (item.isFavorite) {
-      htmlString += `<div><h3>${item.name}</h3><p>${
-        item.source
-      }</p><p>${item.tags.join(", ")}</p></div>`;
+  for (key in json.recipes) {
+    let value = json.recipes[key];
+    if (value.isFavorite) {
+      htmlString += `<div class="recipe-thumbnail" data-recipeid="${key}"><h3 class="thumbnail-name">${
+        value.name
+      }</h3><img class="thumbnail-img" src="images/recipe/${key}.png" alt="${
+        value.name
+      }"/><p class="thumbnail-source">From: ${
+        value.source
+      }</p><p class="thumbnail-tags">${value.tags.join(", ")}</p></div>`;
     }
   }
   document.querySelector("#fave-results").innerHTML = htmlString;
 }
+
+// Get the modal
+var modal = document.getElementById("myModal");
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var widgets = document.getElementsByClassName("widget");
+var modalDiv = document.getElementById("modal-div");
+// for (item of widgets) {
+//   item.onclick = function () {
+//     modal.style.display = "block";
+//     modalDiv.innerHTML = this.innerHTML;
+//   };
+// }
+// Get the <span> element that closes the modal
+var closeButton = document.getElementsByClassName("close")[0];
+// When the user clicks on <span> (x), close the modal
+closeButton.onclick = function () {
+  modal.style.display = "none";
+};
+
+fromLink = "recipes/recipes.json"; // "https://allenjandrew.github.io/wdd130/recipea/recipes/recipes.json";
+window.addEventListener("load", usingJSON(fromLink));
